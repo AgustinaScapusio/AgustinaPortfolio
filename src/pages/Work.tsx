c3f305877141f3ace579562f356aee30faa72c45
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-13 14:04:23 +0100

    Add styling and button for Challenge App

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -28,9 +28,7 @@ export default function Work() {
                 <Button text='Management App' onClick={() => window.open('https://management-project-six.vercel.app/', '_blank')} />
                 <Button text='Quiz App' onClick={() => window.open('https://quiz-app-two-lovat.vercel.app/', '_blank')} />
                 <Button text='Placepicker' onClick={() => window.open('https://placepicker-psi.vercel.app/', '_blank')} />
-                
-
-
+                <Button text='Challenge App' onClick={() => window.open('https://animating-framemotion.vercel.app/', '_blank')} />
             </div>
         </div>
     );
